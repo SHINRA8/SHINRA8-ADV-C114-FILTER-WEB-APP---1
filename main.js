ea9088e5f1398a6869d7ce24a364678c5909c655
SHINRA8 <148629745+SHINRA8@users.noreply.github.com>
2023-10-24 11:13:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+function setup(){
+  function setup() 
+{
+    canvas = createCanvas(280,280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(clasifyCanvas);
+}
+}
+function draw() {
+
+}
+function save_filltered_image(){
+  
+}
